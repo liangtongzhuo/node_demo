b1eb0fdcffd76141a6b056e0f59304d44495159b
hi363138911 <liangya363138911@126.com>
2017-06-16 17:22:22 +0800

    updata

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+/*
+
+在linux下
+只设置一次有效 $ PORT=1234 node xxoo.js
+只设置永久有效  $ export PORT=1234
+在window下
+默认是永久情况
+set PORT=1234
+node xxoo.js
+
+*/
+
+
+//获取的是字符串
+ parseInt(process.env.PORT);
+ 
