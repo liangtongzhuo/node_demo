cb1ad206f1958eb5bf831b42d6e9ae3d9446848c
hi363138911 <liangya363138911@126.com>
2017-07-21 08:43:03 +0800

    update

--- index.js
+++ index.js
@@ -1,85 +1,78 @@
 'use strict'
 
-// var app = require('http').createServer(handler)
-// var io = require('socket.io')(app);
-// var fs = require('fs');
+function compose(middleware){
+  return function *(next){
+    if (!next) next = noop();
 
-// app.listen(8080);
+    var i = middleware.length;
+    // 组合中间件
+    while (i--) {
+      next = middleware[i].call(this, next);
+    }
 
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
+    return yield *next;
+  }
+}
+function *noop(){}
 
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// }
+const middlewares = [];
 
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
+const getTestMiddWare = (loggerA, loggerB) => {
+    return function *(next) {
+        console.log(loggerA);
+        yield next;
+        console.log(loggerB);
+    }
+};
+const mid1 = getTestMiddWare(1, 4),
+    mid2 = getTestMiddWare(2, 3);
 
-// var a ={ x: 1} ;
-// var b = a;
-// a.x  = a = { x: 2 }
+const getData = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('数据已经取出'), 1000);
+});
 
-
-// console.log(a.x);
-// console.log(b.x);
-
-
-// var add = function() {
-//     var sum = 0;
-//     for(var i = 0; i < arguments.length; i++)
-//         sum += arguments[i];
-//     var addMore = function(b) {
-//         for(var j = 0; j < arguments.length; j++)
-//             sum += arguments[j];
-//         return addMore;
-//     };
-//     addMore.toString = function() {
-//         console.log('aaaa');
-//         return sum;
-//     };
-//     return addMore;
-// };
-
-// console.log(add(1,3));
-// console.log(add(1)(3));
-
-
-//  var aaa = {
-//         i: 10,
-//         valueOf: function() { return this.i+30; },
-//         // toString: function() { return this.valueOf()+10; }
-//       }
-//        console.log(+aaa);
-
-
-// console.log(typeof new String('aaaaaa'));
-
- 
-// console.log(typeof  String('hello') );
-// console.log(new String('hello') );
-// console.log(new String('aaaaa') instanceof String);
-
-
-// console.log(typeof 'sss');
-
-// function a(){
-//     arguments
-// }
-
-
-function Person (){
-
+function *response(next) {
+    // 模拟异步读取数据库数据
+    const data = yield getData;
+    console.log(data);
 }
 
-return
+middlewares.push(mid1, mid2, response);
+// 简单模拟co库
+function co(gen) {
+    const ctx = this,
+        args = Array.prototype.slice.call(arguments, 1);
+    return new Promise((reslove, reject) => {
+        if (typeof gen === 'function') gen = gen.apply(ctx, args);
+        if (!gen || typeof gen.next !== 'function') return resolve(gen);
+
+        const baseHandle = handle => res => {
+            let ret;
+            try {
+                ret = gen[handle](res);
+            } catch(e) {
+                reject(e);
+            }
+            next(ret);
+        };
+        const onFulfilled = baseHandle('next'),
+            onRejected = baseHandle('throw');
+            
+        onFulfilled();
+        function next(ret) {
+            if (ret.done) return reslove(ret.value);
+            // 将yield的返回值转换为Proimse
+            let value = null;
+            if (typeof ret.value.then !== 'function') {
+                value = co(ret.value);
+            } else {
+                value = ret.value;
+            }
+            if (value) return value.then(onFulfilled, onRejected);
+            return onRejected(new TypeError('yield type error'));
+        }
+    });
+}
+// 调用方式
+const gen = compose(middlewares);
+co(gen);
---
+++ io.js
@@ -0,0 +1,83 @@
+'use strict'
+
+// var app = require('http').createServer(handler)
+// var io = require('socket.io')(app);
+// var fs = require('fs');
+
+// app.listen(8080);
+
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
+
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
+
+// var a ={ x: 1} ;
+// var b = a;
+// a.x  = a = { x: 2 }
+
+
+// console.log(a.x);
+// console.log(b.x);
+
+
+// var add = function() {
+//     var sum = 0;
+//     for(var i = 0; i < arguments.length; i++)
+//         sum += arguments[i];
+//     var addMore = function(b) {
+//         for(var j = 0; j < arguments.length; j++)
+//             sum += arguments[j];
+//         return addMore;
+//     };
+//     addMore.toString = function() {
+//         console.log('aaaa');
+//         return sum;
+//     };
+//     return addMore;
+// };
+
+// console.log(add(1,3));
+// console.log(add(1)(3));
+
+
+//  var aaa = {
+//         i: 10,
+//         valueOf: function() { return this.i+30; },
+//         // toString: function() { return this.valueOf()+10; }
+//       }
+//        console.log(+aaa);
+
+
+// console.log(typeof new String('aaaaaa'));
+
+ 
+// console.log(typeof  String('hello') );
+// console.log(new String('hello') );
+// console.log(new String('aaaaa') instanceof String);
+
+
+// console.log(typeof 'sss');
+
+// function a(){
+//     arguments
+// }
+
+
+
+
+
